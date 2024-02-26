3834ea8a2b0c9b2c7187806c4192dcd95041dffe
chudishe <vitmanarina.v@gmail.com>
2024-02-26 15:53:20 +0200

    1: made database, User entity and migrated to Version20240226133749

---
+++ src/Entity/User.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?DateTimeImmutable $registrationDate = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $profileDescription = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $profilePicture = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRegistrationDate(): ?DateTimeImmutable
+    {
+        return $this->registrationDate;
+    }
+
+    public function getProfileDescription(): ?string
+    {
+        return $this->profileDescription;
+    }
+
+    public function setProfileDescription(?string $profileDescription): static
+    {
+        $this->profileDescription = $profileDescription;
+
+        return $this;
+    }
+
+    public function getProfilePicture(): ?string
+    {
+        return $this->profilePicture;
+    }
+
+    public function setProfilePicture(string $profilePicture): static
+    {
+        $this->profilePicture = $profilePicture;
+
+        return $this;
+    }
+}
